6468bd9dab389c9348948413c198fe11b411ea7d
CurryMentos <446169496@qq.com>
2020-11-30 16:59:44 +0800

    update

--- web_crawler/test_weather_forecast_demo.py
+++ web_crawler/test_weather_forecast_demo.py
@@ -20,9 +20,9 @@ class TestCraw(object):
         return measure_time
 
     # 中国天气预报
-    @timefn
-    def test_craw(object):
-        url = "http://www.weather.com.cn/textFC/beijing.shtml#"
+    def weather_forecast(object, province):
+        # url = "http://www.weather.com.cn/textFC/beijing.shtml#"
+        url = "http://www.weather.com.cn/textFC/{}.shtml#".format(province)
         headers = {
             'Connection': 'close',
             'Accept''': 'application/json, text/javascript, */*; q=0.01',
@@ -42,15 +42,24 @@ class TestCraw(object):
         province = bs.find('div', class_="conMidtab3")
         province_name = province.find('td', width="74").text
         print('{}(市)天气预报：'.format(province_name))
-        for all_cities in province.find_all('tr'):
-            city_name = all_cities.find('td', width="83").text
-            city_day_weather = all_cities.find('td', width="89").text
-            city_night_weather = all_cities.find('td', width="98").text
-            city_highest_temperature = all_cities.find('td', width="92").text
-            city_lowest_temperature = all_cities.find('td', width="86").text
-            print('{}(区/县)：'.format(city_name))
-            print('白天{} 夜晚{}'.format(city_day_weather, city_night_weather))
-            print('最高温度：{}℃ 最低温度：{}℃'.format(city_highest_temperature, city_lowest_temperature))
+        # for all_cities in province.find_all('tr'):
+        #     city_name = all_cities.find('td', width="83").text
+        #     city_day_weather = all_cities.find('td', width="89").text
+        #     city_night_weather = all_cities.find('td', width="98").text
+        #     city_highest_temperature = all_cities.find('td', width="92").text
+        #     city_lowest_temperature = all_cities.find('td', width="86").text
+        #     print('{}(区/县)：'.format(city_name))
+        #     print('白天{} 夜晚{}'.format(city_day_weather, city_night_weather))
+        #     print('最高温度：{}℃ 最低温度：{}℃'.format(city_highest_temperature, city_lowest_temperature))
+        #     print('===========================================')
+
+    @timefn
+    def test_craw(self):
+        # province = ['beijing', 'tianjin', 'hebei', 'shanxi', 'neimenggu', 'heilongjiang', 'jilin', 'liaoning',
+        #             'shanghai', 'anhui', 'jiangsu', 'shandong', 'zhejiang', 'fujian', 'jiangxi']
+        province = ['heilongjiang']
+        for i in province:
+            self.weather_forecast(i)
 
 
 if __name__ == '__main__':
