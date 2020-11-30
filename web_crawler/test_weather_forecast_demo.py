6ffa5717d56c9b45f9c88ae06a2f21742034191b
CurryMentos <446169496@qq.com>
2020-11-30 16:17:36 +0800

    update

--- web_crawler/test_weather_forecast_demo.py
+++ web_crawler/test_weather_forecast_demo.py
@@ -19,33 +19,39 @@ class TestCraw(object):
 
         return measure_time
 
-    # 哔哩哔哩放映厅
+    # 中国天气预报
     @timefn
     def test_craw(object):
-        url = "https://www.bilibili.com/movie/"
+        url = "http://www.weather.com.cn/textFC/beijing.shtml#"
         headers = {
             'Connection': 'close',
-            # 'Accept''': 'application/json, text/javascript, */*; q=0.01',
-            'Accept''': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept''': 'application/json, text/javascript, */*; q=0.01',
+            # 'Accept''': '*/*',
+            'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'zh-CN,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6',
             # 'Cookie': 'DFF0015216D06A1B48DC7BE3AD3BA45E8|5af2dd013253498be4567c7ab005658a',
-            'Origin': 'https://www.bilibili.com/',
+            # 'Origin': 'https://www.bilibili.com/',
+            # 'Host': 'www.weather.com.cn',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
             # 'Upgrade-Insecure-Requests': '1',
         }
         response = requests.get(url, headers=headers)
         bs = BeautifulSoup(response.content, 'lxml')
 
-        all_movies = bs.find('div', id="app")
-        print('哔哩哔哩放映厅热播推荐：')
-        for each_movie in all_movies.find_all('li', class_="video-item-biref cinema-large"):
-            all_a_tag = each_movie.find_all('a')
+        print('中国天气预报：')
+        province = bs.find('div', class_="conMidtab3")
+        province_name = province.find('td', width="74").text
+        print('{}(市)天气预报：'.format(province_name))
+        for all_cities in province.find_all('tr'):
+            city_name = all_cities.find('td', width="83").text
+            city_day_weather = all_cities.find('td', width="89").text
+            city_night_weather = all_cities.find('td', width="98").text
+            city_highest_temperature = all_cities.find('td', width="92").text
+            city_lowest_temperature = all_cities.find('td', width="86").text
+            print('{}(区/县)：'.format(city_name))
+            print('白天{} 夜晚{}'.format(city_day_weather, city_night_weather))
+            print('最高温度：{}℃ 最低温度：{}℃'.format(city_highest_temperature, city_lowest_temperature))
 
-            movie_name = all_a_tag[0].find('div', class_="biref-title").text
-            url_to_fetch = all_a_tag[0]['href']
 
-            print('{} {}'.format(movie_name, url_to_fetch))
-
-    if __name__ == '__main__':
-        pytest.main(['-s', '-v', 'test_movie_demo.py'])
+if __name__ == '__main__':
+    pytest.main(['-s', '-v', 'test_weather_forecast_demo.py'])
