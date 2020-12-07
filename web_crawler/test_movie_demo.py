09813178a35139ec951588fa269a442333378cb9
CurryMentos <446169496@qq.com>
2020-12-07 16:06:51 +0800

    update

--- web_crawler/test_movie_demo.py
+++ web_crawler/test_movie_demo.py
@@ -23,18 +23,7 @@ class TestCraw(object):
     @timefn
     def test_craw(object):
         url = "https://www.bilibili.com/movie/"
-        headers = {
-            'Connection': 'close',
-            # 'Accept''': 'application/json, text/javascript, */*; q=0.01',
-            'Accept''': '*/*',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'zh-CN,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6',
-            # 'Cookie': 'DFF0015216D06A1B48DC7BE3AD3BA45E8|5af2dd013253498be4567c7ab005658a',
-            'Origin': 'https://www.bilibili.com/',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
-            # 'Upgrade-Insecure-Requests': '1',
-        }
-        response = requests.get(url, headers=headers)
+        response = requests.get(url)
         bs = BeautifulSoup(response.content, 'lxml')
 
         all_movies = bs.find('div', id="app")
